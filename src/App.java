2c98ebc667fefe886b04c15b395ad57aa8f85682
Dan Vitoriano <vitoriano08@gmail.com>
2023-03-28 19:23:21 -0300

    added java code

--- src/App.java
+++ src/App.java
@@ -1,8 +1,20 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
         // fazer uma conexão HTTP e buscar os Top 250 filmes
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
 
         // extrair só os dados que interessam (título, poster, classificação)
 
