22f2ffa4e16c91f4653dc10f18e33ddb09091330
Dan Vitoriano <vitoriano08@gmail.com>
2023-03-28 18:34:42 -0300

    arquivos iniciais

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        // fazer uma conexão HTTP e buscar os Top 250 filmes
+
+        // extrair só os dados que interessam (título, poster, classificação)
+
+        // exibir e manipular os dados
+    }
+}
