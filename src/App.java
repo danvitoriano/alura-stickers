9c984608680b8cbaecaebd2dd2dbda4a1dad65d4
Dan Vitoriano <vitoriano08@gmail.com>
2023-03-29 15:57:44 -0300

    added json parser and map list

--- src/App.java
+++ src/App.java
@@ -3,6 +3,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -18,6 +20,17 @@ public class App {
 
         // extrair só os dados que interessam (título, poster, classificação)
 
+        var parser = new JsonParser();
+        List<Map<String,String>> listaDeFilmes = parser.parse(body);
+        // System.out.println(listaDeFilmes.size());
+        // System.out.println(listaDeFilmes.get(0));
+
         // exibir e manipular os dados
+        for (Map<String, String> filme : listaDeFilmes){
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRatingCount"));
+            System.out.println();
+        }
     }
 }
